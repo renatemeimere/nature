74d0db155c717e074455911c7fd31105351c7dc1
Renate Meimere <renatemeimere@laptop-71281jme.kea.local>
2019-02-25 10:48:32 +0100

    new version

--- js/java.js
+++ js/java.js
@@ -1,14 +1,40 @@
 	const template = document.querySelector("template").content;
 	const main = document.querySelector("main");
+    const nav = document.querySelector("nav");
 	const modal = document.querySelector(".modal-bg");
 	modal.addEventListener("click", () => modal.classList.add("hide"));
 
 	const wonderlistLink = "https://spreadsheets.google.com/feeds/list/1mY0R6Pxy4YpeXaJK08oZxdxriVnXJ-eCfPnbu6iskUc/od6/public/values?alt=json"
 
 	function loadJSON(link) {
-	    fetch(link).then(e => e.json()).then(data => data.feed.entry.forEach(showSingleCard));
+	    fetch(link).then(e => e.json()).then(data => {
+            data.feed.entry.forEach(createCategory)
+            data.feed.entry.forEach(showSingleCard)
+        });
 	}
+    function createCategory(row){
+        /*<section id='nature'>
+            <h2 class="nature">Nature</h2>
+        </section>*/
+        const oldSection=document.querySelector('#'+row.gsx$category.$t)
+        if(oldSection){
+            console.log(row.gsx$category.$t + ' already exists')
+        } else {
+            const section = document.createElement("section");
+            section.id=row.gsx$category.$t;
+            const header = document.createElement('h2');
+            header.textContent=row.gsx$category.$t;
+            section.appendChild(header);
+            main.appendChild(section)
 
+            const link = document.createElement('a');
+            link.textContent=row.gsx$category.$t;
+            link.href="#"+row.gsx$category.$t;
+            nav.appendChild(link)
+        }
+
+
+    }
     function showDetails(oneObject){
         modal.querySelector(".modal-name").textContent = oneObject.gsx$title.$t;
         modal.querySelector(".modal-wonderimg").src = "images/" + oneObject.gsx$image.$t;
@@ -22,7 +48,9 @@
     }
 
 	function showSingleCard(oneObject) {
+
 	    console.log(oneObject)
+
 	    let clone = template.cloneNode(true);
 	    clone.querySelector(".name").textContent = oneObject.gsx$title.$t;
 	    clone.querySelector(".TypeOfWonder").textContent = oneObject.gsx$typeofwonder.$t;
@@ -32,7 +60,9 @@
         clone.querySelector("button").addEventListener("click", () => {
             showDetails(oneObject);
         })
-        main.appendChild(clone);
+
+        const section = document.querySelector("#"+oneObject.gsx$category.$t);
+        section.appendChild(clone);
 
     }
 loadJSON(wonderlistLink);
